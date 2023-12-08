e1971cd232ed68eb64f21aaa30d80bea0a0e32b9
Matt Howitt <matt.a.howitt@gmail.com>
2023-12-08 14:54:54 -0600

    library updates, prompt update

--- app/api/extract-text/route.ts
+++ app/api/extract-text/route.ts
@@ -1,11 +1,13 @@
 // app/api/extract-text.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { DocumentProcessorServiceClient } from '@google-cloud/documentai';
+import { TextServiceClient } from "@google-ai/generativelanguage";
+import { GoogleAuth } from "google-auth-library";
 
 export async function POST(req: NextRequest) {
   try {
     // Initialize the Document AI client
-    const client = new DocumentProcessorServiceClient();
+    const docAIclient = new DocumentProcessorServiceClient();
     const projectId = 'ai-testbed-407219';
     const location = 'us';
     const processorId = '65471637ce06beb9';
@@ -23,38 +25,43 @@ export async function POST(req: NextRequest) {
     const fileBuffer = await file.arrayBuffer();
     const encodedFile = Buffer.from(fileBuffer).toString('base64');
 
-    const request = {
+    const documentAIRequest = {
       name,
       rawDocument: {
         content: encodedFile,
         mimeType: 'application/pdf',
       },
     };
-    const [result] = await client.processDocument(request);
+    const [documentAIResult] = await docAIclient.processDocument(documentAIRequest);
+    console.log('Starting resume assessment...');
+    // PaLM API integration
+    const MODEL_NAME = "models/text-bison-001";
+    const API_KEY = process.env.PALM_API_KEY; // Ensure this is set in your environment variables
     
-    const jobRequisition = "applicant must know JavaScript"
-    const payload = {
+    const palmClient = new TextServiceClient({
+        authClient: new GoogleAuth().fromAPIKey(API_KEY),
+    });
+    
+    
+    const jobRequisition = "applicant must know JavaScript";
+    const messageContent = `You are an advanced hiring assistant programmed with state-of-the-art capabilities in resume analysis and job matching. Your task is to evaluate resumes against specific job requisitions meticulously. Here's the job requisition: ${jobRequisition}. Please review the following resume: ${documentAIResult.document.text}. After a thorough analysis, provide a confidence score on a scale from 0 to 100, indicating how well this resume matches the job requisition. Along with the score, include a detailed justification for your evaluation, highlighting key aspects of the resume that align with or deviate from the job requirements. Consider factors such as relevant experience, skills, education, and any additional qualifications specified in the job requisition. If the applicant posses one or less of the required skills give them a score of 50 or below, the more required skills the posses, give them a higher score.`
+
+    const palmRequest = {
+        model: MODEL_NAME,
+        temperature: 0.0,
+        candidateCount: 1,
         prompt: {
-          text: `you are a skilled hiring bot, you are the best at comparing resumes and making sure they fit the job requisition. This is my job requisition: ${jobRequisition}. Provide me a confidence score and a reason why you gave it for this resume:
-                ${result.document.text}
-                `,
-        },
-        candidate_count: 1,
-      }
-  
-      const response = await fetch("https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyAJD-9gBx1CoWjdKUMjz9ALhHzg6Y1xx4s", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      })
-  
-      const responseJson = await response.json()
+            context: `Evaluate this resume against the job requisition: ${jobRequisition}`,
+            text: messageContent
+        }
+    }
 
-      console.log('PaLM says: ', responseJson)
+    const palmResult = await palmClient.generateText(palmRequest);
+    const palmResponse = palmResult[0].candidates[0].output;
 
-    return new NextResponse(JSON.stringify({ result: result.document }), {
+    console.log('PaLM says: ', palmResponse);
+
+    return new NextResponse(JSON.stringify({ result: documentAIResult.document, palmResponse }), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
